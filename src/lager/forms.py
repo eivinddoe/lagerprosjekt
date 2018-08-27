dbf5ca4d06c164a045d1cb157d9e157ace8036c6
Eivind K. Dovik <eivind.doe@gmail.com>
2018-08-27 22:57:01 +0200

    mvp near complete

--- src/lager/forms.py
+++ src/lager/forms.py
@@ -1,15 +1,17 @@
+from bootstrap_datepicker_plus import DatePickerInput
 from django import forms
 import datetime
 
 class MvpForm(forms.Form):
 	pris = forms.IntegerField(label='Pris')
-	leveringstid = forms.IntegerField(label='Leveringstid')
-	sist_byttet = forms.DateField(label='Sist byttet', initial=datetime.date.today)
-	levetid = forms.IntegerField(label='Levetid')
+	leveringstid = forms.IntegerField(label='Leveringstid (uker)')
+	sist_byttet = forms.DateField(label='Sist byttet', input_formats=['%d/%m/%Y'],
+		widget=DatePickerInput(format='%d/%m/%Y'))
+	levetid = forms.IntegerField(label='Levetid (år)')
 
 	KONSEKVENSER = (
 		("Driftsavbrudd", "Driftsavbrudd"),
-		("Strømproduksjon", "Strømdproduksjon"),
+		("Strømproduksjon", "Strømproduksjon"),
 		("Ingen avbrudd", "Ingen avbrudd"),
 	)
 
--- src/lager/views.py
+++ src/lager/views.py
@@ -1,23 +1,100 @@
+from datetime import datetime, timedelta
 from django.shortcuts import render
+
+import matplotlib
+matplotlib.use('agg')
+import matplotlib.pyplot as plt
+
+import math
+import numpy as np
 
 from .forms import MvpForm
 
 # Create your views here.
 def main(request):
-	if request.method == 'POST':
-		form = MvpForm(request.POST)
-		if form.is_valid():
-			context = {
-			'pris': form.cleaned_data['pris'],
-			'leveringstid': form.cleaned_data['leveringstid'],
-			'sist_byttet': form.cleaned_data['sist_byttet'],
-			'levetid': form.cleaned_data['levetid'],
-			'konsekvens': form.cleaned_data['konsekvens'],
-			'form': form
-			}
-	else:
-		form = MvpForm()
-		context = {
-		'form': form
-		}	
-	return render(request, "index.html", context)
+    lagerkost = 0.05
+    today = datetime.today().date()
+    context = {
+	'today': today
+	}
+    if request.method == 'POST':
+        form = MvpForm(request.POST)
+        if form.is_valid():
+            levetid_dager = form.cleaned_data['levetid'] * 365
+            tid_inne = (today-form.cleaned_data['sist_byttet']).days
+            konsekvens = form.cleaned_data['konsekvens']
+            pris = form.cleaned_data['pris']
+            lagerkost_dag = pris * lagerkost/365
+
+            context.update({
+            'pris': pris,
+            'lagerkost_dag': lagerkost_dag,
+            'leveringstid': form.cleaned_data['leveringstid'],
+            'leveringstid_dager': form.cleaned_data['leveringstid'] * 7,
+            'sist_byttet': form.cleaned_data['sist_byttet'],
+            'tid_inne': tid_inne,
+            'levetid': form.cleaned_data['levetid'],
+            'levetid_dager': levetid_dager,
+            'konsekvens': form.cleaned_data['konsekvens'],
+            'form': form,
+            'analyse': True
+            })
+            
+            # Function to compute Weibull cumulative density function
+            def WeibullCDF(x, lmbd, k):
+                q = pow(x/lmbd, k)
+                return 1.0 - math.exp(-q)
+            
+            # Beregne vektet risiko (nedetidskostnad)
+            if konsekvens == 'Driftsavbrudd':
+            	nedetidskostnad_dag = 550000
+            elif konsekvens == 'Strømproduksjon':
+            	nedetidskostnad_dag = 107000
+            else:
+            	nedetidskostnad_dag = 0
+
+            cdf_start = WeibullCDF(tid_inne, levetid_dager, 5)
+            survival = 1-cdf_start
+            
+            probabilities = []
+            for i in range(tid_inne + 1, levetid_dager + 1):
+                probabilities.append((WeibullCDF(i, levetid_dager, 5)-cdf_start)/survival)
+
+            vektet_risiko = [i * nedetidskostnad_dag for i in probabilities]
+            
+            context.update({
+            	'cdf_start': cdf_start,
+            	'vektet_risiko': vektet_risiko
+            	})
+
+            # Beregne vektet lagerkostnad
+            probabilities_complement = [(1-i) for i in probabilities]
+            vektet_lagerkost_dag = [i * lagerkost_dag for i in probabilities_complement]
+            vektet_lagerkost = []
+            for i in range(1,len(vektet_lagerkost_dag)+1):
+                vektet_lagerkost.append(i * vektet_lagerkost_dag[i-1])
+
+            context.update({
+            	'vektet_lagerkost': vektet_lagerkost
+            	})
+
+            # Finn kritisk dag
+            i = 0
+            while vektet_risiko[i] < vektet_lagerkost[i]:
+            	i += 1
+
+            kritisk_dato = today + timedelta(days=i)
+
+            context.update({
+            	'kritisk_dag': i,
+            	'kritisk_dato': kritisk_dato
+            	})
+
+
+
+    else:
+        form = MvpForm()
+        context = {
+        'form': form
+        }
+    return render(request, "index.html", context)
